d5a0baea2a7b326a02efd9aca3fcacc3fb1cc907
peter <peter.yudhistiraa@gmail.com>
2023-06-01 17:17:34 +0700

    [1/6/2023] Overtime, saving progress and doc so i won't get fucked if something happens

--- Finalized/db.py
+++ Finalized/db.py
@@ -4,9 +4,11 @@ import os
 
 
 class DatabaseHandler():
-    def __init__(self, dbName):
+    def __init__(self, dbName, keepSchema: bool = True):
         self.cursor = sqlite3.connect(dbName)
-        self.create_tables()
+        if not keepSchema:
+            print('ya schema empty, yeet')
+            self.create_tables()
 
     # create
     def create_tables(self):
@@ -25,7 +27,7 @@ class DatabaseHandler():
                 datetime TEXT NOT NULL
             );
         """)
-
+        # self.cursor.execute("DROP TABLE record_data") # the sacred seal of the nine tails. Do not open
         self.cursor.execute("""
                 CREATE TABLE IF NOT EXISTS record_data (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,7 +35,8 @@ class DatabaseHandler():
                 speaker_id INTEGER,
                 question_text NOT NULL,
                 transcript_audio TEXT NOT NULL,
-                transcript_text TEXT NOT NULL
+                transcript_text TEXT NOT NULL,
+                transcribe_lang TEXT NOT NULL
             );
         """)
 
@@ -55,17 +58,17 @@ class DatabaseHandler():
         # create the directory.
         os.mkdir("records/event{}".format(id))
 
-    def create_record_audio(self, event_id, speaker_id, question, audio_path):
+    def create_record_audio(self, event_id, speaker_id, question, audio_path, transcribe_lang):
         self.cursor.execute("""
-                INSERT INTO record_data (event_id, speaker_id, question_text, transcript_audio, transcript_text)
-                VALUES (?, ?, ?, ?, "")
-        """, (event_id, speaker_id, question, audio_path))
+                INSERT INTO record_data (event_id, speaker_id, question_text, transcript_audio, transcript_text, transcribe_lang)
+                VALUES (?, ?, ?, ?, "", ?)
+        """, (event_id, speaker_id, question, audio_path, transcribe_lang))
         self.cursor.commit()
 
     def create_record_text(self, event_id, speaker_id, question, text):
         self.cursor.execute("""
-                INSERT INTO record_data (event_id, speaker_id, question_text, transcript_audio, transcript_text)
-                VALUES (?, ?, ?, "", ?)
+                INSERT INTO record_data (event_id, speaker_id, question_text, transcript_audio, transcript_text, transcribe_lang)
+                VALUES (?, ?, ?, "", ?, "")
         """, (event_id, speaker_id, question, text))
         self.cursor.commit()
 
@@ -87,7 +90,7 @@ class DatabaseHandler():
                 SELECT * FROM record_data
                 WHERE {} = ?
         """.format(selector)
-        self.cursor.execute(query, (ID,)).fetchall()
+        return self.cursor.execute(query, (ID,)).fetchall()
 
     # list
 
@@ -106,15 +109,11 @@ class DatabaseHandler():
                 SELECT * FROM record_data
         """).fetchall()
 
-    # definitive record data
-    def list_recordData_complete(self):
-        pass
-
     # update
     def update_recordData_text(self, ID, text):
         self.cursor.execute("""
                 UPDATE record_data
-                SET text = ?
+                SET transcript_text = ?
                 WHERE id = ?
         """, (text, ID))
         self.cursor.commit()
@@ -126,10 +125,14 @@ class DatabaseHandler():
         """.format(table))
         self.cursor.commit()
 
-    # goofy ahh functions
-    def get_recordCountByEventID(self, eventID):
-        return len(self.get_recordData("event_id", eventID))
+    def delete_by_id(self, table:str, id:int):
+        self.cursor.execute("""
+            DELETE FROM {}
+            WHERE ID = ?
+        """.format(table), (id))
+        self.cursor.commit()
 
+    # goofy ahh functions
     def loop_create_speaker(self):
         # # create speakers
         while True:
@@ -139,21 +142,10 @@ class DatabaseHandler():
             code = input("Code : ")
             self.create_speaker(name, code)
 
-    def create_recordDataFromExcel(self, filePath, sheetName, eventID):
-        df = pd.read_excel(filePath, sheet_name=sheetName)
-        for i in df.index:
-            print("-" * 80)
-            speakerCode = df.loc[i]["Speaker"]
-            print(speakerCode)
-            speakerData = self.get_speaker("code", speakerCode)
-            speakerID = speakerData[0]
 
-            self.create_record_text(event_id=eventID, speaker_id=speakerID, question=df.loc[i]["Question"], text=df.loc[i]["Answer"])
-            print("-" * 80)
-            
     def get_recordDataJoined(self, selector, ID):
         query = """
-                SELECT record_data.id as id, events.title as event_title, events.datetime as date, speakers.code as speaker_code, question_text, transcript_audio, transcript_text
+                SELECT record_data.id as id, events.title as event_title, events.datetime as date, speakers.code as speaker_code, question_text, transcript_audio, transcript_text, transcribe_lang
                 FROM record_data
                 INNER JOIN events
                 ON record_data.event_id = events.id
@@ -162,7 +154,7 @@ class DatabaseHandler():
                 WHERE record_data.{} = ?
         """.format(selector)
         return self.cursor.execute(query, (ID,)).fetchall()
-    
+
     def get_recordDataJoinedDF(self, selector, ID):
         query = """
                 SELECT record_data.id as id, events.title as event_title, speakers.code as speaker, question_text as question, transcript_text as answer
@@ -177,10 +169,14 @@ class DatabaseHandler():
         return df
 
 
-# dh = DatabaseHandler("database/testdb.db")
-# dh.clear_table("record_data")
-# dh.create_recordDataFromExcel("database/raws/Dataset.xlsx", "19-Feb", 18)
-# dh.create_recordDataFromExcel("database/raws/Dataset.xlsx", "26-Feb", 19)
-# dh.create_recordDataFromExcel("database/raws/Dataset.xlsx", "2-Jul", 20)
-# print(dh.get_recordDataJoined("event_id", 10))
-# print(dh.get_recordData("event_id", 11))
+# dh = DatabaseHandler("testdb.db")
+# dh.delete_by_id("record_data", 97)
+
+# dh.update_recordData_text(118, "Uh, this, actually, according to me, if we backtrack, if we are talking limitations, limits surely have something that triggers it or sets it. There is something that makes that the limitations exist. Example, because Peter was a programmer, he makes an AI, or a program. Well, AI can only move within the computer. Well, the computer can only exists within the scope of the hardware of the computer. The hardware cannot exist if nobody makes it. We can pull it, backtrack it. Hardware needs machine. Machine will not exist if there is no maker. The maker is human, let's pull it there. Human cannot exist if there is no time, no what, no dimensions, no physics. We pull and pull, and i think eventually it will end up somewhere, an origin. Otherwise we will just spin around, which is, it does not make sense, like that. Because this system of backtrack has like a hierarchy, where the bottom cannot change the top. Computer cannot do things to humans unless that human makes it do what he says. But if not ordered, the computer will not do anything. This is not just causality, but there is a hierarchy, where the bottom cannot do things to the top. There is a corner, an end, at some point. For me it's like that, pad.")
+# dh.update_recordData_text(119, "OK, thank you ko. Yeah, i wanted to communicate that, but my words are missing. So for example computer, i have screen here, i have CPU here, they are limited by the components. For example, my screen is 21 inches, and RGB something. And my CPU, the spec is, the RAM is 16 giga, and the GPU is RTX, or GTX, i forget. And this computer is limited by those rooms, those scopes, so that it cannot get out of those scopes. My screen can't just shrink to 15 inches, or get big to 40 inches. CPU cannot go up to 32 giga, and GTX cannot go up to RTX. Cannot, unless i change it myself. Unless, that which is outside the limits make intervention and change what is within the limits.")
+# dh.update_recordData_text(120, "As a person who follows post-modernism, yeah, post-modernism is a little troublesome. To me, yeah, hierarchy is determined by humans, isn't it? We categorize by ourself, like soup A 1, soup A 2, this atmosphere, atmosphere A B C, this and that dimension, such is human. Well me, because i am a post-modernist, i think that what if humans think like that because that is all we know. Maybe it is askew from reality. One day maybe atmosphere will turn out to have 30 layers, not 7. We do not know. We can only search and search. About hierarchy, it may exist, but not certain. I think this because, if you know, i see the story of Warhammer 40k. Turns out there is cosmic dimension that makes them able to cross dimensions. But then they meet Chaos gods. Turns out the god they worship is atheist, actually. Just a man that is powerful. From there i learn that it may be that we are being lied to. The famous social philosophy is everything is illusion. But Peter is right, i guess. For instance Artificial intelligence, it works because of the data that we give, photos that we give, footages that we give. Imagine if god makes it like that, that what if god makes us think that he is like that, while he isn't. Just like a paradox, i forget, about the creation. I don't know the exact answer. It is mysterious, one of the world's mysteriousness.")
+# dh.update_recordData_text(121, "Uh, the limits are bound by humans, that it is imaginary. For me, perhaps, maybe, i attribute it to human limitation, because of human's limitations of understanding. Because it is not something that humans are meant to understand, that the limitations are there and absolute. But, there are things that are beneath us. For instance, about programming, hierarchy is absolute. User is beyond everything, and below user is a program, and below program is a language, and below language is the binary, that is the bits and 0 1 0 1. In the hierarchy that is below us, i can bear witness, i can vouch that that which is below cannot influence that which is above. So i feel sufficiently confident to think that there are limitations above me that i cannot comprehend fully, that i cannot transcend. And above it, there is another, and another, until there is a scope that transcends everything, the whole universe.")
+# dh.update_recordData_text(122, "I believe that, what is it, based on what i see below me, that cannot interact with me above it, there exists limitations. Like this, humans want a lot of things, but program does not understand what human wants, so the media between human and program exists, that is the programming language. I cannot make a program that understands human emotions, because, yeah, it is hard to communicate it to program. That is why, i believe that there is a principle above us, that we cannot understand, that i cannot comprehend. To me, this universal principle, i believe that it is god. This is what i believe as a person with religion. This is based on what is within scope cannot comprehend that which is outside the scope. I cannot understand the principles of life, why gravity, laws of nature, and physics are like that, i don't understand. I do not understand that which is outside of my scope, unless that which is outside of the scope explains to me in a language that i understand. That it makes contact with me willingly, with, say, writings, words, yeah, or something that i can see and understand. Oh, so it is like that.")
+# dh.update_recordData_text(123, "The counter, that is, i found on TikTok, that everything is illusion. It's all in our heads. All that we speak of is illusion. A philosopher said that we are living in a dream, in a hologram, in a simulation. So we have been talking like we are smoking weed. Also, it doesn't mean that when we find a hierarchy, that it really is a hierarchy, or that there is god. Because Peter said about artificial intelligence, if a person's brain is put inside an artificial intelligence, it could be that his arguments are revolving, in a paradox. Who is creating who, who is creating God, it could be like that. The point is it is mysterious. Even though i am Christian and cannot think like that, that it is heretical. But the world is vast, so now we can think such and so. People can be more open minded, the science is advanced, and now we know about how consciousness works. So, i see on TikTok, that when a person dies, there is a brain, right, if the brain is preserved and taken care of, the brain can be alive again, as long as it is not rotting. It lives again. So imagine that your brain is revived, but with no body. Imagine that, and now we know how consciousness works. We know how brain and biology works. But essence, we have no milestone. Otherwise the paradox begins again, like Big Bang, when it happens, whether God is there or not. So i don't know, it just revolves like that.")
+# dh.update_recordData_text(124, "If we say between knowing and not, basically, in the end, we only know what we know. We only know what we know. Yeah. Outside of that, we don't know. I heard that somewhere, whatever. So, we only know what we know. For me, for me, we can know how does what is above us work, for example time. We know how time works, we know it flows. There is flow of time, that we know. We know that the world of physics, it works in dimensional ways. We have 3 dimensions, and so on. I don't understand that fully, but the point is, we can be aware of something is above us. But i know that time is outside of our scope, because we cannot create time. We cannot create in quote, a dimension. We can live in a dimension, or create things in a dimension, but we cannot create a dimension. Or we cannot create time. So time and dimension is higher in the hierarchy. But we know how it works. So, in the end, we can know that which is above us, but only that. We cannot understand it, never know.")
+# dh.update_recordData_text(125, "Because, as Samuel Sugiarto says, the theology professor of Petra, that humans are below humans. Which is, that is his belief, yeah. This is small intermezzo, for his genre of Christianity, it is below him. We did not create animals, yeah, Peter, but the majority think that animals are below them. Ko cep always says that animals are below us, does not have free will, or consciousness like us, et cetera. But it turns out recent studies prove that animals are like humans, but just less skillful in it. For example, animals like sex. Turns out not only humans like sex. Animals like sex too, for example. The primates find certain movements that can be enjoyed sexually. Cats and dogs are too. In certain degree, animals are exactly like humans. Animals like violence, even ants fight each other because of different skin color. Not even artificial intelligence, even animals that are below us are not different from us. Only different in shape, but same in principle. I question what should i believe now, because if i believe Mr Samuel, animals are within control. But as i read, animals are not within control. Maybe god is like that, maybe god cannot be read as such. No, no, that is bad. The point is, the more we know, turns out, the more we don't know. It could be that we know God, but in that, we don't know him even more.")
